1b2501b7fdae7dcd23352ea1fdc8ef0944614738
Michele Faedi <Michele.Faedi@studio.unibo.it>
2020-05-24 16:14:39 +0200

    added commitment

---
+++ commitment.js
@@ -0,0 +1,60 @@
+var functions = require("./util.js");
+
+const service = "commitment";
+
+class commitment {
+    constructor(store) {
+        this.store = store;
+        this.service = service;
+        this.commitment = store.getTable("MyCommitment");
+        this.step = store.getTable("MyStep");
+        this.stepDone = store.getTable("MyStepDone");
+        this.lastCommitmentId = functions.getBestInArray(this.commitment, "idCommitment", (id1, id2) => id1 - id2).idCommitment;
+        this.lastStepId = functions.getBestInArray(this.step, "idMyStep", (id1, id2) => id1 - id2).idMyStep;
+    }
+
+    async manageGet(queryString) { }
+
+    async managePost(body) {
+        let idUser = this.store.getIdUser(body.idSession);
+        if (body.method == "addCommitment" && body.data.commitment != undefined && Array.isArray(body.data.steps)) {
+            this.lastCommitmentId++;
+            this.commitment.push({
+                idCommitment: this.lastCommitmentId,
+                name: body.data.commitment.name, desc: body.data.commitment.desc, creationDate: new Date(), idUser: idUser
+            });
+            body.data.steps.array.forEach(e => {
+                this.lastStepId++;
+                this.step.push({
+                    idMyStep: this.lastStepId, idCommitment: this.lastCommitmentId,
+                    name: e.name, unitMeasure: e.unitMeasure, max: e.max, repetitionDay: e.repetitionDay, type: e.type
+                });
+                this.stepDone.push({ idMyStep: this.lastStepId, dateStart: new Date(), result: 0 });
+            });
+
+            this.store.saveData("MyCommitment", this.commitment);
+            this.store.saveData("MyStep", this.step);
+            this.store.saveData("MyStepDone", this.stepDone);
+        } else if (body.method == "updateMyStepDone" && body.data.idMyStep != undefined) {
+            let max = undefined;
+            let maxI = -1;
+            for (let i = 0; i < this.stepDone; i++) {
+                if (this.stepDone[i].idMyStep == body.data.idMyStep) {
+                    if (max == undefined || this.stepDone[i].dateStart > max) {
+                        max = this.stepDone[i].dateStart;
+                        maxI = i;
+                    }
+                }
+            }
+            this.stepDone[maxI].result = parseInt(body.data.result);
+            this.store.saveData("MyStepDone", this.stepDone);
+        } else if (body.method == "addMyStepDone" && body.data.idMyStep != undefined) {
+            if (this.store.searchKey("MyStep", "idMyStep", body.data.idMyStep) != undefined) {
+                this.stepDone.push({ idUser: idUser, dateStart: new Date(), result: 0 });
+                this.store.saveData("MyStepDone", this.stepDone);
+            }
+        }
+    }
+}
+
+module.exports = commitment
